9b13824f0d821ad3a759d79240fabb9e3d4b88ac
Daan Scheerens <d.scheerens@gmail.com>
2022-07-18 11:34:46 +0200

    chore: update dependencies

--- demo/app/app.module.ts
+++ demo/app/app.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ExtendedAsyncPipeModule } from 'ngx-extended-async-pipe';
 
 import { AppComponent } from './app.component';
@@ -10,7 +9,6 @@ import { AppComponent } from './app.component';
     imports: [
         BrowserModule,
         CommonModule,
-        BrowserAnimationsModule,
         ExtendedAsyncPipeModule.withUndefinedAsDefault(),
     ],
     declarations: [
